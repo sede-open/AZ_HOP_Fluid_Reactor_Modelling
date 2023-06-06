24bdee3eebb6493038b21638178b53d2d89def3a
Victor Gamayunov <vgamayunov@microsoft.com>
2023-06-06 18:15:30 +0000

    disable NHC shutdown (for debugging)

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/99-healthcheck.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/99-healthcheck.sh
@@ -4,8 +4,8 @@ errormessage=$( /usr/sbin/nhc 2>&1)
 error=$?
 
 # In case of health check failure, shutdown the node by calling the script /usr/libexec/nhc/azhop-node-offline.sh
-if [ $error -eq 1 ]; then
-    /usr/libexec/nhc/azhop-node-offline.sh $(hostname) "$errormessage"
-    JETPACK=/opt/cycle/jetpack/bin/jetpack
-    $JETPACK shutdown --unhealthy
-fi
+# if [ $error -eq 1 ]; then
+#     /usr/libexec/nhc/azhop-node-offline.sh $(hostname) "$errormessage"
+#     JETPACK=/opt/cycle/jetpack/bin/jetpack
+#     $JETPACK shutdown --unhealthy
+# fi
